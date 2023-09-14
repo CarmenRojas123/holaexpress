03717a75d5ae38f75805e1687b4a29761ee39003
CarmenRojas123 <rojascarmen641@gmail.com>
2023-09-13 21:17:46 -0600

    Mi segundo commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+response.send('Hola Carmen, si salio el trabajo')
+})
+/**
+ * 
+ * 
+ * FUNCIONALIDAD
+ * 
+ *
+ *  
+ */
+
+//http://localhost:3000/
+
+app.listen(3000, () =>{
+console.log('listening on port 3000')
+
+})
