608f9b32869d88fb7d3cc594aaffcda90bedd035
CarmenRojas123 <144382714+CarmenRojas123@users.noreply.github.com>
2023-09-15 10:30:35 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+response.send('HOLA GET')
+})
+app.get('/', (request, response) => {
+    response.send('HOLA POST')
+    })
+
+/**
+ * 
+ * 
+ * FUNCIONALIDAD
+ * 
+ *
+ *  
+ */
+
+app.listen(3000, () =>{
+console.log('listening on port 3000')
+
+})
